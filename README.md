8872ca957c492c5554750efc8ba187d5d866885d
myhang <nguyenhoangmyhang1209@gmail.com>
2022-05-31 16:32:49 +0700

    push lan 2

--- README.md
+++ README.md
@@ -68,3 +68,17 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+src
+|__ components (shared components between features)
+|  |__ Loading
+|     |__ index.jsx
+|     |__ styles.scss
+|
+|__ features
+|  |__ Todo
+|     |__ components (components of feature Todo)
+|     |__ pages (pages of feature Todo)
+|     |__ index.jsx (entry point of feature Todo)
+|
+|__ App.js
--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const name = "hang";
+  const age = 18;
+  const isFemale = true;
+  const student = {
+    name: 'my hang'
+  };
+  const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My hang 1209
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>xin chao {name} - {age} - {isFemale ? "Female" : "male"} </p>
+
+        {isFemale ? <p>Female</p> : <p>male</p>}
+        {isFemale && <p>Female</p>}
+        {!isFemale && <p>Male</p>}
+
+        { isFemale && (
+          <div>
+            <p>Female 1</p>
+            <p>Female 2</p>
+            <p>Female 3</p>
+          </div>
+        )}
+
+        { isFemale && (
+          <>
+            <p>Female 1</p>
+            <p>Female 2</p>
+            <p>Female 3</p>
+          </>
+        )}
+
+        <p>{student.name}</p>
+
+          <ul>
+            {colorList.map(color => (
+              // map() để list ra một danh sách từ dữ liệu ban đầu.
+              <li key = {color} style={{color}}>{color}</li>
+            ))}
+          </ul>
+
       </header>
     </div>
   );
