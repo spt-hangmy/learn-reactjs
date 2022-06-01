6d6387932340c697156c5eeafe859a5ced8a4034
myhang <nguyenhoangmyhang1209@gmail.com>
2022-06-01 08:40:30 +0700

    commit lan 3

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import TodoFeatures from './features/Todo';
 
 function App() {
-  const name = "hang";
-  const age = 18;
-  const isFemale = true;
-  const student = {
-    name: 'my hang'
-  };
-  const colorList = ['red', 'green', 'blue'];
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My hang 1209
-        </p>
-
-        <p>xin chao {name} - {age} - {isFemale ? "Female" : "male"} </p>
-
-        {isFemale ? <p>Female</p> : <p>male</p>}
-        {isFemale && <p>Female</p>}
-        {!isFemale && <p>Male</p>}
-
-        { isFemale && (
-          <div>
-            <p>Female 1</p>
-            <p>Female 2</p>
-            <p>Female 3</p>
-          </div>
-        )}
-
-        { isFemale && (
-          <>
-            <p>Female 1</p>
-            <p>Female 2</p>
-            <p>Female 3</p>
-          </>
-        )}
-
-        <p>{student.name}</p>
-
-          <ul>
-            {colorList.map(color => (
-              // map() để list ra một danh sách từ dữ liệu ban đầu.
-              <li key = {color} style={{color}}>{color}</li>
-            ))}
-          </ul>
-
-      </header>
+      <TodoFeatures/>
     </div>
   );
 }
